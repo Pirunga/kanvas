85ab236b435c15d8958f95d64daab8589ed57042
Luiz Matheus <luizmatheusga@gmail.com>
2021-05-11 00:46:31 -0300

    feat: add activities/<int:user_id> route at users urls

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,12 @@ from users.views import (
     UserView,
     CourseView,
     ActivityView,
+    ActivityFilterByStudentView,
 )
 
 urlpatterns = [
     path("accounts/", UserView.as_view()),
     path("courses/", CourseView.as_view()),
     path("activities/", ActivityView.as_view()),
+    path("activities/<int:user_id>", ActivityFilterByStudentView.as_view()),
 ]
