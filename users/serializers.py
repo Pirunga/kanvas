c01feeae1d4dcc729b0900d94e383cf89742c97f
Luiz Matheus <luizmatheusga@gmail.com>
2021-05-10 21:25:53 -0300

    feat: add CourseStudentsSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.Serializers):
@@ -13,3 +14,17 @@ class CourseSerializer(serializers.Serializers):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     user_set = UserSerializer(read_only=True, many=True)
+
+
+class CourseStudentsSerializer(serializers.Serializer):
+    users_id = serializers.ListField(child=serializers.IntegerFIeld())
+
+    def validate_users_ids(self, value):
+        for user_id in value:
+            user = User.objects.get(id=user_id)
+
+            if user:
+                return True
+
+            else:
+                raise serializers.ValidationError("Student/Facilitador not found")
