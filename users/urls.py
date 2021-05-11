7adc299e3d55e125e2ffe2e2ca1de351896c430f
Luiz Matheus <luizmatheusga@gmail.com>
2021-05-11 00:29:41 -0300

    feat: add CourseView and ActivityView in users urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from users.views import UserView
+from users.views import (
+    UserView,
+    CourseView,
+    ActivityView,
+)
 
 urlpatterns = [
     path("accounts/", UserView.as_view()),
+    path("courses/", CourseView.as_view()),
+    path("activities/", ActivityView.as_view()),
 ]
